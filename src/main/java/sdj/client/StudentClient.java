0a7ef18bc6996b8d3742312a71af09c490f8bc3c
Abhinav <abhinav1408@Abhinavs-MacBook-Air.local>
2026-09-08 22:08:15 +0530

    learned save and update method in detail.

---
+++ src/main/java/sdj/client/StudentClient.java
@@ -0,0 +1,29 @@
+package sdj.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import sdj.entity.Student;
+import sdj.repository.StudentRepository;
+
+@Component
+public class StudentClient implements ApplicationRunner {
+	
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {		
+		Student student = new Student("Alisa Simmons", "2022AN50123");
+		Student returnedStudent = studentRepository.save(student);
+		returnedStudent.setName("Alissa Simmons");
+		Student updatedStudent = studentRepository.save(returnedStudent);
+		System.out.println("Updated Student: " + updatedStudent);
+		System.out.println(returnedStudent);
+		System.out.println(studentRepository.findById(updatedStudent.getId()));
+
+	}
+
+}
