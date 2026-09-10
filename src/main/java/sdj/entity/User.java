919e820012231c1185c8377a9572a4cb15640dc5
abhinav1408 <abhinav83423@gmail.com>
2026-09-10 13:33:56 +0530

    learned about query by example to fetch data from datasource.

--- src/main/java/sdj/entity/User.java
+++ src/main/java/sdj/entity/User.java
@@ -14,19 +14,24 @@ public class User {
     @Column(unique = true)
     private String username;
 
+    private String firstname;
+    private String lastname;
+
     @Column(name="registration_date")
     private LocalDate registrationDate;
 
     private String email;
-    private Integer level;
+    private int level;
 
     @Column(name="active")
     private Boolean isActive;
 
     public User() {
     }
-    public User(String username, LocalDate registrationDate, String email, Integer level, Boolean isActive) {
+    public User(String username, String firstname, String lastname, LocalDate registrationDate, String email, Integer level, Boolean isActive) {
         this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.registrationDate = registrationDate;
         this.email = email;
         this.level = level;
@@ -36,5 +41,41 @@ public class User {
     // getters and setters for username
     public String getUsername() {
         return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    // getters and setters for firstname and lastname
+    public String getFirstname() {
+        return firstname;
+    }
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public Integer getLevel() {
+        return level;
+    }
+    //to string method
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", registrationDate=" + registrationDate +
+                ", email='" + email + '\'' +
+                ", level=" + level +
+                ", isActive=" + isActive +
+                '}';
     }
 }
--- src/test/java/sdj/MainApplication2Tests.java
+++ src/test/java/sdj/MainApplication2Tests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import sdj.entity.User;
@@ -26,13 +28,14 @@ public class MainApplication2Tests {
     @BeforeAll
     public void setup() {
         //add dummy users using userRepository.save() method
-        userRepository.save(new User("user1", LocalDate.of(2022, 1, 1), "user1@example.com", 1, true));
-        userRepository.save(new User("user2", LocalDate.of(2022, 2, 1), "user2@example.com", 2, false));
-        userRepository.save(new User("user3", LocalDate.of(2022, 3, 1), "user3@example.com", 3, true));
-        userRepository.save(new User("user4", LocalDate.of(2022, 4, 1), "user4@example.com", 4, false));
-        userRepository.save(new User("user5", LocalDate.of(2022, 5, 1), "user5@example.com", 5, true));
-        userRepository.save(new User("user6", LocalDate.of(2022, 6, 1), "user6@example.com", 6, false));
-
+        userRepository.save(new User("user1", "John", "Brown", LocalDate.of(2022, 1, 1), "user1@example.com", 1, true));
+        userRepository.save(new User("user2", "Jane", "Smith", LocalDate.of(2022, 2, 1), "user2@example.com", 2, false));
+        userRepository.save(new User("user3", "Bob", "Johnson", LocalDate.of(2022, 3, 1), "user3@example.com", 3, true));
+        userRepository.save(new User("user4", "Alice", "Williams", LocalDate.of(2022, 4, 1), "user4@example.com", 4, false));
+        userRepository.save(new User("user5", "Charlie", "Brown", LocalDate.of(2022, 5, 1), "user5@example.com", 2, true));
+        userRepository.save(new User("user6", "David", "Davis", LocalDate.of(2022, 6, 1), "user6@example.com", 6, false));
+        userRepository.save(new User("user7", "Eve", "Miller", LocalDate.of(2022, 7, 1), "user7@example.com", 7, true));
+        userRepository.save(new User("user8", "Frank", "Wilson", LocalDate.of(2022, 8, 1), "user8@example.com", 8, false));
     }
     @AfterAll
     public void cleanup() {
@@ -46,5 +49,67 @@ public class MainApplication2Tests {
         List<User> users = page.getContent();
         assertThat(users.size()).isEqualTo(3);
         assertThat(users.get(2).getUsername()).isEqualTo("user3");
+    }
+    //Query BY Example
+    @Test
+    public void testQueryByExample1(){
+        User user = new User();
+        user.setLastname("Smith");
+        user.setLevel(2);
+
+        Example example = Example.of(user);
+        List<User> users = userRepository.findAll(example);
+        System.out.println("Found users: " + users);
+//        assertThat(users.size()).isEqualTo(1);
+
+    }
+    @Test
+    public void testQueryByExample2() {
+        User user = new User();
+        user.setLastname("Smith");
+//        user.setLevel(2);
+
+        Example example = Example.of(user);
+        List<User> users = userRepository.findAll(example);
+        System.out.println("Found users: " + users);
+
+    }
+    @Test
+    public void testQueryByExample3() {
+        User user=new User();
+        user.setLastname("Brown");
+
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("level");
+        Example example = Example.of(user, matcher);
+        List<User> users = userRepository.findAll(example);
+        System.out.println("Found users: " + users);
+    }
+    @Test
+    public void testQueryByExample4() {
+        User user=new User();
+        user.setFirstname("j");
+
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("level")
+                .withStringMatcher(ExampleMatcher.StringMatcher.STARTING)
+                .withIgnoreCase();
+        Example example = Example.of(user, matcher);
+        List<User> users = userRepository.findAll(example);
+        System.out.println("Found users: " + users);
+    }
+    @Test
+    public void testQueryByExample5() {
+        User user = new User();
+        user.setFirstname("j");
+        user.setLastname("wn");
+
+        ExampleMatcher matcher = ExampleMatcher.matchingAny()
+                .withIgnorePaths("level")
+                .withMatcher("firstname",(match)->match.startsWith())
+                .withMatcher("lastname",(match)->match.endsWith())
+                .withIgnoreCase();
+        List<User> users = userRepository.findAll(Example.of(user, matcher));
+        System.out.println("Found users: " + users);
     }
 }
